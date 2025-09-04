c322bf580ff260c988720d9e97df548025a8bf40
kurita <kurita@plus1jp.com>
2025-09-04 17:23:16 +0900

    設定修正

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://timestudy_db_7yhl_user:0y6Tmj2464cgvxUDfNbwGNINxXvCuILA@dpg-d1sp1v6mcj7s73aovdh0-a.singapore-postgres.render.com/timestudy_db_7yhl")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://timestudy_db_7yhl_user:0y6Tmj2464cgvxUDfNbwGNINxXvCuILA@dpg-d1sp1v6mcj7s73aovdh0-a.singapore-postgres.render.com/timestudy_db_7yhl?sslmode=require")
 print("📡 DATABASE_URL:", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
